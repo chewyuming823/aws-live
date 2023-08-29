3c533442ebd503ab37dcce002c8c0328a71de04c
chewyuming823 <143489518+chewyuming823@users.noreply.github.com>
2023-08-29 15:41:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cgxdldmvlb5k.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chewyuming-employee"
+customregion = "us-east-1"
+
